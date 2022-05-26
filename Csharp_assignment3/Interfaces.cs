ac740a607fcb45557c61933c23ba9a185feb576e
666wen <105310343+666wen@users.noreply.github.com>
2022-05-26 10:12:24 -0700

    update assignment3

---
+++ Csharp_assignment3/Interfaces.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_assignment3
+{
+    public interface Interfaces
+    {
+        public void ICourseService();
+        public void IStudentService();
+
+        public void IInstructorService();
+        public void IDepartmentService();
+        public void IPersonService();
+
+    }
+}
